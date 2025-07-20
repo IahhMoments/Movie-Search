edcf32ee4f901b6aa0021799d06d8695ce716452
IahhMoments <isaiahchhay0@gmail.com>
2025-07-20 16:40:39 -0500

    added rendered content when searching movie

--- index.js
+++ index.js
@@ -6,14 +6,26 @@ const fetchData = async (searchTerm) => {
     },
   });
 
-  console.log(response.data.Search);
+  return response.data.Search;
 };
 
 const input = document.querySelector("input");
 
 const onInput = async (event) => {
   const movies = await fetchData(event.target.value);
-  console.log(movies);
+
+  for (let movie of movies) {
+    const div = document.createElement("div");
+
+    //content of how the movies are going to be rendered
+    div.innerHTML = `
+    <img src="${movie.Poster}" />
+    <h1>${movie.Title}</h1>
+    `;
+
+    //render movies by selecting div with ID of 'target' and appending div variable as a child
+    document.querySelector("#target").appendChild(div);
+  }
 };
 
-input.addEventListener("input", debounce(onInput, 1000));
+input.addEventListener("input", debounce(onInput, 500));
