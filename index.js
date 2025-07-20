1543b8307004e9814b16cf0bf96e9945260591d7
IahhMoments <isaiahchhay0@gmail.com>
2025-07-20 15:53:19 -0500

    added a limiter for user searches so it does not overload the network requests

--- index.js
+++ index.js
@@ -9,7 +9,18 @@ const fetchData = async (searchTerm) => {
   console.log(response.data);
 };
 
+let timeOutId;
 const input = document.querySelector("input");
-input.addEventListener("input", (event) => {
-  fetchData(event.target.value);
-});
+const onInput = (event) => {
+  //clear Id number for setTimeout
+  if (timeOutId) {
+    clearTimeout(timeOutId);
+  }
+
+  //runs everytime user presses a key in input and calls new Id for setTimeout
+  timeOutId = setTimeout(() => {
+    fetchData(event.target.value);
+  }, 1000);
+};
+
+input.addEventListener("input", onInput);
