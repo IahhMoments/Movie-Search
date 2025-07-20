90225c1b4807aaca21e7e0def4a7727a0ac50c17
IahhMoments <isaiahchhay0@gmail.com>
2025-07-20 17:29:14 -0500

    rendered movie content as a dropdown selection for all movies for given search query

--- index.js
+++ index.js
@@ -13,22 +13,37 @@ const fetchData = async (searchTerm) => {
   return response.data.Search;
 };
 
+const root = document.querySelector(".autocomplete");
+root.innerHTML = `
+  <label><b>Search For a Movie</b></label>
+  <input class="input" />
+  <div class="dropdown">
+    <div class="dropdown-menu">
+      <div class="dropdown-content results"></div>
+    </div>
+  </div>
+`;
+
 const input = document.querySelector("input");
+const dropdown = document.querySelector(".dropdown");
+const resultsWrapper = document.querySelector(".results");
 
 const onInput = async (event) => {
   const movies = await fetchData(event.target.value);
 
+  dropdown.classList.add("is-active");
   for (let movie of movies) {
-    const div = document.createElement("div");
+    const selection = document.createElement("a");
 
     //content of how the movies are going to be rendered
-    div.innerHTML = `
+    selection.classList.add("dropdown-item");
+    selection.innerHTML = `
     <img src="${movie.Poster}" />
-    <h1>${movie.Title}</h1>
+    ${movie.Title}
     `;
 
     //render movies by selecting div with ID of 'target' and appending div variable as a child
-    document.querySelector("#target").appendChild(div);
+    resultsWrapper.appendChild(selection);
   }
 };
 
